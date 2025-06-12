b9d3c61198b0e9f4c450dbbba19f12b3b04ad320
aryanajay07 <ajay.201340@ncit.edu.np>
2025-06-12 11:50:58 +0545

    updated models

---
+++ speed_estimation/__init__.py
@@ -0,0 +1,1 @@
+# This file makes the speed_estimation directory a Python package 
---
+++ speed_estimation/config.py
@@ -0,0 +1,7 @@
+vehicle_detection_model_path='models/yolov8m.pt'
+license_detection_model_path='models/license_detection_model.pt'
+license_plate_segmentation_model_path='models/license_plate_segmentation_model.pt'
+classifier_model_path='models/classifier_model.pth'
+speed_limit=1
+# Common vehicle class names in COCO
+VEHICLE_CLASSES = ['car', 'truck', 'bus', 'motorbike', 'motorcycle']
